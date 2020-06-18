bcefabfebbf8d229e5ec39e0f615f65ed843cca1
xjtuerz0 <iamzhouling@163.com>
2020-06-18 22:26:21 +0800

    code with notes

--- 2-tensorflow-glove/tf_glove.py
+++ 2-tensorflow-glove/tf_glove.py
@@ -14,6 +14,7 @@ class NotFitToCorpusError(Exception):
 class GloVeModel():
     def __init__(self, embedding_size, context_size, max_vocab_size=100000, min_occurrences=1,
                  scaling_factor=3/4, cooccurrence_cap=100, batch_size=512, learning_rate=0.05):
+        # 定义所有参数
         self.embedding_size = embedding_size
         if isinstance(context_size, tuple):
             self.left_context, self.right_context = context_size
@@ -33,16 +34,19 @@ class GloVeModel():
         self.__embeddings = None
 
     def fit_to_corpus(self, corpus):
+        # 调用共现矩阵构建函数以及调用向量与共现矩阵近似关系的函数
         self.__fit_to_corpus(corpus, self.max_vocab_size, self.min_occurrences,
                              self.left_context, self.right_context)
         self.__build_graph()
 
     def __fit_to_corpus(self, corpus, vocab_size, min_occurrences, left_size, right_size):
+        # 构建共现矩阵
         word_counts = Counter()
         cooccurrence_counts = defaultdict(float)
         for region in corpus:
             word_counts.update(region)
             for l_context, word, r_context in _context_windows(region, left_size, right_size):
+                # 在上下文窗口内滑动统计单词j在单词i中出现的次数
                 for i, context_word in enumerate(l_context[::-1]):
                     # add (1 / distance from focal word) for this pair
                     cooccurrence_counts[(word, context_word)] += 1 / (i + 1)
@@ -59,32 +63,40 @@ class GloVeModel():
             if words[0] in self.__word_to_id and words[1] in self.__word_to_id}
 
     def __build_graph(self):
+        # 构建向量与共现矩阵近似关系
+        # 创建TensorFlow图环境，用于处理数据流
         self.__graph = tf.Graph()
+         # 查看设备为CPU还是GPU
         with self.__graph.as_default(), self.__graph.device(_device_for_node):
+           # 参数初始化
             count_max = tf.constant([self.cooccurrence_cap], dtype=tf.float32,
                                     name='max_cooccurrence_count')
+            # 设置共现矩阵最大值
             scaling_factor = tf.constant([self.scaling_factor], dtype=tf.float32,
                                          name="scaling_factor")
-
+            # 设置缩放因子
             self.__focal_input = tf.placeholder(tf.int32, shape=[self.batch_size],
                                                 name="focal_words")
+            # 关键词
             self.__context_input = tf.placeholder(tf.int32, shape=[self.batch_size],
                                                   name="context_words")
+            # 上下文窗口内的单词变量
             self.__cooccurrence_count = tf.placeholder(tf.float32, shape=[self.batch_size],
                                                        name="cooccurrence_count")
-
+            # 统计上下文窗口内在单词i周围出现的词的频率
             focal_embeddings = tf.Variable(
                 tf.random_uniform([self.vocab_size, self.embedding_size], 1.0, -1.0),
                 name="focal_embeddings")
+            # 上下文窗口内的词向量大小
             context_embeddings = tf.Variable(
                 tf.random_uniform([self.vocab_size, self.embedding_size], 1.0, -1.0),
                 name="context_embeddings")
-
+            # 关键词损失函数偏置
             focal_biases = tf.Variable(tf.random_uniform([self.vocab_size], 1.0, -1.0),
                                        name='focal_biases')
             context_biases = tf.Variable(tf.random_uniform([self.vocab_size], 1.0, -1.0),
                                          name="context_biases")
-
+            # 查询三类词的初始向量，用于后面的AdaGrad梯度下降训练
             focal_embedding = tf.nn.embedding_lookup([focal_embeddings], self.__focal_input)
             context_embedding = tf.nn.embedding_lookup([context_embeddings], self.__context_input)
             focal_bias = tf.nn.embedding_lookup([focal_biases], self.__focal_input)
@@ -97,18 +109,19 @@ class GloVeModel():
                     scaling_factor))
 
             embedding_product = tf.reduce_sum(tf.multiply(focal_embedding, context_embedding), 1)
-
+            # 对共现概率取对数，防止概率相乘最后得到的值为0
             log_cooccurrences = tf.log(tf.to_float(self.__cooccurrence_count))
-
+            # 对共现概率之和开方，用于求解损失函数
             distance_expr = tf.square(tf.add_n([
                 embedding_product,
                 focal_bias,
                 context_bias,
                 tf.negative(log_cooccurrences)]))
-
+            # 构建损失函数
             single_losses = tf.multiply(weighting_factor, distance_expr)
             self.__total_loss = tf.reduce_sum(single_losses)
             tf.summary.scalar("GloVe_loss", self.__total_loss)
+            # 最小化平方差损失函数
             self.__optimizer = tf.train.AdagradOptimizer(self.learning_rate).minimize(
                 self.__total_loss)
             self.__summary = tf.summary.merge_all()
@@ -118,12 +131,15 @@ class GloVeModel():
 
     def train(self, num_epochs, log_dir=None, summary_batch_interval=1000,
               tsne_epoch_interval=None):
+        # 训练模型，首先读取本地日志路径以及参数
         should_write_summaries = log_dir is not None and summary_batch_interval
         should_generate_tsne = log_dir is not None and tsne_epoch_interval
         batches = self.__prepare_batches()
         total_steps = 0
         with tf.Session(graph=self.__graph) as session:
+            # 建立TensorFlow会话
             if should_write_summaries:
+                # 将编译日志写入本地路径
                 print("Writing TensorBoard summaries to {}".format(log_dir))
                 summary_writer = tf.summary.FileWriter(log_dir, graph=session.graph)
             tf.global_variables_initializer().run()
@@ -151,12 +167,14 @@ class GloVeModel():
                 summary_writer.close()
 
     def embedding_for(self, word_str_or_id):
+        # 获取词向量索引
         if isinstance(word_str_or_id, str):
             return self.embeddings[self.__word_to_id[word_str_or_id]]
         elif isinstance(word_str_or_id, int):
             return self.embeddings[word_str_or_id]
 
     def __prepare_batches(self):
+        # 读取初始共现概率矩阵并调用batchify函数将矩阵转化为元组
         if self.__cooccurrence_matrix is None:
             raise NotFitToCorpusError(
                 "Need to fit model to corpus before preparing training batches.")
@@ -167,26 +185,31 @@ class GloVeModel():
 
     @property
     def vocab_size(self):
+        # 获取字典大小
         return len(self.__words)
 
     @property
     def words(self):
+        # 检测数据集是否为空
         if self.__words is None:
             raise NotFitToCorpusError("Need to fit model to corpus before accessing words.")
         return self.__words
 
     @property
     def embeddings(self):
+        # 检测是否产生训练结果
         if self.__embeddings is None:
             raise NotTrainedError("Need to train model before accessing embeddings")
         return self.__embeddings
 
     def id_for_word(self, word):
+        # 如果单词没有ID则调用索引函数进行转化
         if self.__word_to_id is None:
             raise NotFitToCorpusError("Need to fit model to corpus before looking up word ids.")
         return self.__word_to_id[word]
 
     def generate_tsne(self, path=None, size=(100, 100), word_count=1000, embeddings=None):
+        # 用t-sne算法对数据进行降维，为后面将相似的词表示在二维平面内做准备，最后调用_plot_with_labels函数绘图
         if embeddings is None:
             embeddings = self.embeddings
         from sklearn.manifold import TSNE
@@ -197,6 +220,7 @@ class GloVeModel():
 
 
 def _context_windows(region, left_size, right_size):
+    # 调用窗口大小设置函数，设定上下文窗口大小，用于创建共现矩阵
     for i, word in enumerate(region):
         start_index = i - left_size
         end_index = i + right_size
@@ -206,6 +230,8 @@ def _context_windows(region, left_size, right_size):
 
 
 def _window(region, start_index, end_index):
+    # 窗口大小设置
+
     """
     Returns the list of words starting from `start_index`, going to `end_index`
     taken from region. If `start_index` is a negative number, or if `end_index`
@@ -216,7 +242,7 @@ def _window(region, start_index, end_index):
     selected_tokens = region[max(start_index, 0):min(end_index, last_index) + 1]
     return selected_tokens
 
-
+# 检测设备情况，若有GPU则使用GPU进行计算，否则用CPU
 def _device_for_node(n):
     if n.type == "MatMul":
         return "/gpu:0"
@@ -225,11 +251,13 @@ def _device_for_node(n):
 
 
 def _batchify(batch_size, *sequences):
+    # 将矩阵转换为元组
     for i in range(0, len(sequences[0]), batch_size):
         yield tuple(sequence[i:i+batch_size] for sequence in sequences)
 
 
 def _plot_with_labels(low_dim_embs, labels, path, size):
+    # 绘制图形界面，展示训练结果
     import matplotlib.pyplot as plt
     assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
     figure = plt.figure(figsize=size)  # in inches
@@ -239,5 +267,6 @@ def _plot_with_labels(low_dim_embs, labels, path, size):
         plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right',
                      va='bottom')
     if path is not None:
+        # 将绘制出来的图形结果保存到本地
         figure.savefig(path)
         plt.close(figure)
